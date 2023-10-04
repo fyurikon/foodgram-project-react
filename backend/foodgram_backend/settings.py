3f365696bf1dc229e07e5bf3eac7adc1860fc3bb
Yuri Filatov <fyurikon@gmail.com>
2023-10-04 21:23:05 +0300

    Configuring setup.cfg

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'default_key')
 
-DEBUG = os.environ.get('DEBUG','False')=='True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(' ')
 
