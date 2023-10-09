d4a172c741ab2b1347e8e1faa945044d494eb540
Yuri Filatov <fyurikon@gmail.com>
2023-10-09 21:44:13 +0300

    Small fixes.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -35,8 +35,8 @@ class CustomUserSerializer(ModelSerializer):
     def get_is_subscribed(self, obj):
         user = self.context['request'].user
         return (
-            user.is_authenticated and
-            Follow.objects.filter(user=user, following=obj).exists()
+            user.is_authenticated
+            and Follow.objects.filter(user=user, following=obj).exists()
         )
 
 
